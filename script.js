26f6ef1503575dd585c642392403927151bdecd9
Aarush Sharma <aarush.sharma6@gmail.com>
2022-05-17 15:52:20 +0530

    css : added footer

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// lets grab our searchForm, searchResult, whole container
+// and declare an empty searchQuery string
 
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+const APP_ID = "f01324d9";
+const APP_KEY = "86b9a8fa0989a4d70ce734e6aec522d1";
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector("input").value;
+    console.log(searchQuery);
+})
